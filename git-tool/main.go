cfbb7fd3578dfbf5e60c533baf2146ca14e434f9
anishBudha <anishbudha5628@gmail.com>
2025-12-10 17:41:07 -0500

    using porcelain

--- git-tool/main.go
+++ git-tool/main.go
@@ -8,8 +8,23 @@ import (
 	"bufio"
 )
 
-func getGitStatus() (string, []string){
-	cmd := exec.Command("git", "status")
+type GitStatus struct {
+	LocalBranch string
+	RemoteBranch string
+	Untracked []string
+	ModifiedStaged []string
+	ModifiedUnstaged []string
+	Added []string
+	AddedThenModified []string
+	DeletedStaged []string
+	DeletedUnstaged []string
+	ModifiedStagedModified []string
+
+}
+
+func getGitStatus() GitStatus {
+	status := GitStatus{}
+	cmd := exec.Command("git", "status", "--porcelain", "--branch")
 	output, err := cmd.Output()
 
 	lines := strings.Split(string(output), "\n")
@@ -17,34 +32,52 @@ func getGitStatus() (string, []string){
 	if err !=  nil {
 		fmt.Println("error", err)
 	}
-
-	var branch string
-	var untracked []string // slice of strings
-	collecting := false
+	
 
 	for _, line := range lines {
-		if strings.HasPrefix(line, "On branch") {
-			parts := strings.Split(line, " ")
-			branch = parts[len(parts) - 1]
+		if strings.HasPrefix(line, "##") {
+			trimmedLine := strings.TrimPrefix(line, "## ")
+			parts := strings.Split(trimmedLine, "...")
+			status.LocalBranch = parts[0]
+			if len(parts) > 1 {
+				status.RemoteBranch = parts[1]
+			} else {
+				status.RemoteBranch= "no remote"
+			}
 		}
-
-		if strings.HasPrefix(line, "Untracked files:") {
-			collecting = true
+		if len(line) < 3 {
 			continue
 		}
-		if collecting {
-			trimmed := strings.TrimSpace(line)
-			if trimmed == "" {
-				collecting = false
-				continue
-			} 
-			if strings.HasPrefix(trimmed, "(") {
-				continue
-			}
-			untracked = append(untracked, trimmed)
+		if strings.HasPrefix(line, " M") {
+			filename := line[3:]
+			status.ModifiedUnstaged = append(status.ModifiedUnstaged, filename)
+		} else if strings.HasPrefix(line, "M ") {
+			filename := line[3:]
+			status.ModifiedStaged = append(status.ModifiedStaged, filename)
+		} else if strings.HasPrefix(line, "A ") {
+			filename := line[3:]
+			status.Added = append(status.Added, filename)
+		} else if strings.HasPrefix(line, "AM") {
+			filename := line[3:]
+			status.AddedThenModified = append(status.AddedThenModified, filename)
+		} else if strings.HasPrefix(line, "D ") {
+			filename := line[3:]
+			status.DeletedStaged = append(status.DeletedStaged, filename)
+		} else if strings.HasPrefix(line, " D") {
+			filename := line[3:]
+			status.DeletedUnstaged = append(status.DeletedUnstaged, filename)
+		} else if strings.HasPrefix(line, "R "){
+			filename := line[3:]
+			status.Renamed = append(status.Renamed, filename)
+		} else if strings.HasPrefix(line, "MM") {
+			filename := line[3:]
+			status.ModifiedStagedModified = append(status.ModifiedStagedModified, filename)
+		} else if strings.HasPrefix(line, "??") {
+			filename := line[3:]
+			status.Unstaged = append(status.Unstaged, filename)
 		}
 	}
-	return branch, untracked
+	return status
 }
 
 func gitAddAll() string {
@@ -101,11 +134,6 @@ func main () {
 	branch, untracked := getGitStatus()
 	fmt.Println("Branch:", branch)
 	fmt.Println("length of untracked:", len(untracked))
-	fmt.Println("Untracked files:")
-	for _, file := range untracked {
-		fmt.Println(file)
-	}
-
 
 	if untracked != nil {
 		fmt.Println("Untracked files:")
